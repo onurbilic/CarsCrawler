148950c7f639ec8445f784831b7912fd6630b53e
obilic <onur.bilic@hepsiburada.com>
2023-05-22 22:18:52 +0300

    unnessery files

--- src/Services/CarsCrawler/CarsCrawler.Domain/CarsCrawler.Domain/Model/Consts.cs
+++ src/Services/CarsCrawler/CarsCrawler.Domain/CarsCrawler.Domain/Model/Consts.cs
@@ -2,7 +2,7 @@ namespace CarsCrawler.Domain.Model
 {
     public static class Consts
     {
-        public const string testUrl = "https://www.cars.com/";
+        public const string TestUrl = "https://www.cars.com/";
         public const string SearchCarsCommand = "In.Carsdotcom.Search";
         public const string GetCarDetailCommand = "In.Carsdotcom.CarDetail";
 
